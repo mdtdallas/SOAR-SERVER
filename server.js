a3ae445509c5e4b509d09bd1be13f9e2fd89f27f
Dallas Little <mdtdallas@hotmail.com>
2022-06-15 16:08:16 +1000

    front set up

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = express();
 require("dotenv").config();
 
 const corsOptions = {
-  origin: "*",
+  origin: "http://localhost:3000",
   //   origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
